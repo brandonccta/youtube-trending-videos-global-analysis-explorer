4f263054fc5754bd5f52a797cfe8a608eb0363a3
brandonccta <brandonta888@gmail.com>
2026-03-15 03:16:16 -0700

    fixed vercel deployment config

---
+++ api/[...path].js
@@ -0,0 +1,5 @@
+// Vercel serverless function wrapper for Express app
+import app from '../server/index.js';
+
+// Export Express app directly - Vercel supports Express apps natively
+export default app;
